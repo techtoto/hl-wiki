ac5795ca2775db754d77656494dc37c51a6d7073
techtoto <toto.rei0210@gmail.com>
2023-12-15 01:17:50 +0100

    UI basically done

--- main.js
+++ main.js
@@ -1,31 +1,76 @@
-const light_top_left = document.getElementById("licht_oben_links");
-const light_top_right = document.getElementById("licht_oben_rechts");
-const light_middle = document.getElementById("licht_mitte");
-const light_bottom_left = document.getElementById("licht_unten_links");
-const light_bottom_right = document.getElementById("licht_unten_rechts");
-const light_bar_top = document.getElementById("lichtbalken_oben");
-const light_bar_bottom = document.getElementById("lichtbalken_unten");
+const lightTopLeftElement = document.getElementById("licht_oben_links")
+const lightTopRightElement = document.getElementById("licht_oben_rechts")
+const lightMiddleElement = document.getElementById("licht_mitte")
+const lightBottomLeftElement = document.getElementById("licht_unten_links")
+const lightBottomRightElement = document.getElementById("licht_unten_rechts")
+const lightBarTopElement = document.getElementById("lichtbalken_oben")
+const lightBarBottomElement = document.getElementById("lichtbalken_unten")
 
-const light_cycle = [["green", false], ["green", true], ["#3f3f3f", false]]
-var light_cycle_state = 0
-
-function test() {
-    console.log(light_cycle_state);
-
-    light_top_left.style.fill = light_cycle[light_cycle_state][0];
-    
-    if (light_cycle[light_cycle_state][1]) {
-        light_top_left.style.animationPlayState = "running";
-    } else {
-        light_top_left.style.animationPlayState = "stopped";
-        light_top_left.style.fill = light_cycle[light_cycle_state][0];
+class Cycle {
+    constructor(element, color) {
+        this.cycleState = 1
+        this.element = element
+        this.color = color
     }
-    
-    if (light_cycle_state == 2) {
-        light_cycle_state = 0;
-    } else {
-        light_cycle_state++;
+
+    cycle() {
+        console.log(this.cycleState)
+
+        if (this.cycleState == 0) {
+            this.element.style.fill = "#3f3f3f"
+            this.cycleState = 1
+        } else {
+            this.element.style.fill = this.color
+            this.cycleState = 0
+        }
+    }
+
+    get state() {
+        return this.cycleState
     }
 }
 
-light_top_left.addEventListener("click", test);
+class CycleBlink extends Cycle {
+    // only "yellow" and "green" are valid
+    constructor(element, color) {
+        super(element, color)
+    }
+
+    cycle() {
+        console.log(this.cycleState)
+
+        if (this.cycleState == 0) {
+            this.element.classList.remove(this.color + "-blink")
+            this.element.style.fill = "#3f3f3f"
+            this.cycleState = 1
+        } else if (this.cycleState == 1) {
+            this.element.style.fill = this.color
+            this.cycleState = 2
+        } else {
+            this.element.style.fill = "#3f3f3f"
+            this.element.classList.add(this.color + "-blink")
+            this.cycleState = 0
+        }
+    }
+}
+
+const lightTopLeft = new CycleBlink(lightTopLeftElement, "yellow")
+lightTopLeftElement.addEventListener("click", function(){lightTopLeft.cycle()})
+
+const lightTopRight = new CycleBlink(lightTopRightElement, "green")
+lightTopRightElement.addEventListener("click", function(){lightTopRight.cycle()})
+
+const lightMiddle = new Cycle(lightMiddleElement, "red")
+lightMiddleElement.addEventListener("click", function(){lightMiddle.cycle()})
+
+const lightBottomLeft = new Cycle(lightBottomLeftElement, "yellow")
+lightBottomLeftElement.addEventListener("click", function(){lightBottomLeft.cycle()})
+
+const lightBottomRight = new Cycle(lightBottomRightElement, "red")
+lightBottomRightElement.addEventListener("click", function(){lightBottomRight.cycle()})
+
+const lightBarTop = new Cycle(lightBarTopElement, "yellow")
+lightBarTopElement.addEventListener("click", function(){lightBarTop.cycle()})
+
+const lightBarBottom = new Cycle(lightBarBottomElement, "green")
+lightBarBottomElement.addEventListener("click", function(){lightBarBottom.cycle()})
